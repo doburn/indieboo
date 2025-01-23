6c0a203296255562fa79ac44fb4b5a8e15f102ce
doburn <kwon2852@gmail.com>
2025-01-23 20:16:19 +0900

    feat: 백엔드 프로젝트 생성

---
+++ backend/src/test/java/com/indieboo/IndiebooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indieboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiebooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
